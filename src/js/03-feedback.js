8d034943dca52d7036ce5b706e6c5f351dc70817
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-03-09 02:02:13 +0200

    rework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,56 +1,33 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+const feedbackFormData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-// const input = document.querySelector("[name='email']");
-const form = document.querySelector('.feedback-form');
-// const textArea = document.querySelector('[name="message"]');
-
-const feedbackFormState = {};
-let savedFeedbackFormState = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-form.addEventListener('input', throttle(onTextInput, 500));
-//   feedbackFormState[e.target.name] = e.target.value;
-//   console.log(feedbackFormState);
-//   console.log(e.target);
-// );
-
-form.addEventListener('submit', onFormSubmit);
-
-// textArea.addEventListener('input', throttle(onTextAreaInput, 500));
+formRef.addEventListener('input', throttle(onTextInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
 
 populateTextarea();
 
 function onTextInput(e) {
-  feedbackFormState[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+  feedbackFormData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
 }
-
-// function onTextAreaInput(e) {
-//   const message = e.target.value;
-//   localStorage.setItem(STORAGE_KEY, message);
-// }
 
 function onFormSubmit(e) {
   e.preventDefault();
-  e.target.reset();
 
+  console.log(feedbackFormData);
+
+  Object.keys(feedbackFormData).forEach(key => delete feedbackFormData[key]);
+  e.target.reset();
   localStorage.removeItem(STORAGE_KEY);
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-  console.log(savedFeedbackFormState);
 }
 
 function populateTextarea() {
-  if (savedFeedbackFormState) {
-    // textArea.value = savedFeedbackFormState;
-
-    const keys = Object.keys(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-
-    for (const key of keys) {
-      document.querySelector(`[name="${key}"]`).value =
-        savedFeedbackFormState[key];
-      feedbackFormState[key] = savedFeedbackFormState[key];
-    }
-    console.log(feedbackFormState);
+  if (feedbackFormData) {
+    Object.keys(feedbackFormData).forEach(key => {
+      document.querySelector(`[name="${key}"]`).value = feedbackFormData[key];
+    });
   }
 }
