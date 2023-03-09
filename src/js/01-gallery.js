e5a8e6d238d32fd00bfeb2051843a672abdde6eb
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-03-09 02:14:20 +0200

    some change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,7 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items';
 
 const galleryRef = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
